de10484a29ca51c8b89ef45ce82d0e52a07666f9
Vinicius Victorino <sinqia.vinicius@agibank.com.br>
2020-06-13 21:30:15 -0300

    Start container

--- packages/core/src/Container/styles.ts
+++ packages/core/src/Container/styles.ts
@@ -2,4 +2,53 @@ import { Height } from '../Height';
 import { Width } from '../Width';
 import styled from 'styled-components';
 
-export const ContainerStyle = styled(Height.withComponent(Width))``;
+const MAX_SAFE_INTEGER = Math.pow(2, 31) - 1;
+export const ContainerStyle = styled(Height.withComponent(Width))<any>`
+  box-sizing: border-box;
+  ${({ z_max }) => z_max && `z-index: ${MAX_SAFE_INTEGER};`}
+  ${({ z_neg }) => z_neg && 'z-index: -1;'}
+  ${({ z_auto }) => z_auto && 'z-index: auto;'}
+  ${({ z_0 }) => z_0 && 'z-index: 0;'}
+  ${({ z_10 }) => z_10 && 'z-index: 10;'}
+  ${({ z_20 }) => z_20 && 'z-index: 20;'}
+  ${({ z_30 }) => z_30 && 'z-index: 30;'}
+  ${({ z_40 }) => z_40 && 'z-index: 40;'}
+  ${({ z_50 }) => z_50 && 'z-index: 50;'}
+  ${({ s_auto }) => s_auto && 'overflow: auto;'}
+  ${({ s_hidden }) => s_hidden && 'overflow: hidden;'}
+  ${({ s_visible }) => s_visible && 'overflow: visible;'}
+  ${({ sy_auto }) => sy_auto && 'overflow-y: auto;'}
+  ${({ sy_hidden }) => sy_hidden && 'overflow-y: hidden;'}
+  ${({ sy_visible }) => sy_visible && 'overflow-y: visible;'}
+  ${({ sx_auto }) => sx_auto && 'overflow-x: auto;'}
+  ${({ sx_hidden }) => sx_hidden && 'overflow-x: hidden;'}
+  ${({ sx_visible }) => sx_visible && 'overflow-x: visible;'}
+  ${({ fixed }) => fixed && 'position: fixed;'}
+  ${({ absolute }) => absolute && 'position: absolute;'}
+  ${({ relative }) => relative && 'position: relative;'}
+  ${({ sticky }) => sticky && 'position: sticky;'}
+  
+  ${({ top }) => top && 'top: 0;'}
+  ${({ right }) => right && 'right: 0;'}
+  ${({ bottom }) => bottom && 'bottom: 0;'}
+  ${({ left }) => left && 'left: 0;'}
+  ${({ cover }) => cover && 'left: 0;top: 0;right: 0;bottom: 0;'}
+
+  
+`;
+
+// .object-contain	object-fit: contain;
+// .object-cover	object-fit: cover;
+// .object-fill	object-fit: fill;
+// .object-none	object-fit: none;
+// .object-scale-down	object-fit: scale-down;
+
+// .pointer-events-none	pointer-events: none;
+// .outline-none	outline: 0;
+// .cursor-auto	cursor: auto;
+// .cursor-default	cursor: default;
+// .cursor-pointer	cursor: pointer;
+// .cursor-wait	cursor: wait;
+// .cursor-text	cursor: text;
+// .cursor-move	cursor: move;
+// .cursor-not-allowed	cursor: not-allowed;
--- stories/core/Container.test.tsx
+++ stories/core/Container.test.tsx
@@ -4,7 +4,156 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 describe('Container', () => {
-  test('flex', () => {
-    const component = renderer.create(<Container />).toJSON();
+  describe('Z-Index', () => {
+    test('z_max', () => {
+      const MAX_SAFE_INTEGER = Math.pow(2, 31) - 1;
+      const component = renderer.create(<Container z_max />).toJSON();
+      expect(component).toHaveStyleRule('z-index', MAX_SAFE_INTEGER.toString());
+    });
+
+    test('z_neg', () => {
+      const component = renderer.create(<Container z_neg />).toJSON();
+      expect(component).toHaveStyleRule('z-index', '-1');
+    });
+
+    test('z_auto', () => {
+      const component = renderer.create(<Container z_auto />).toJSON();
+      expect(component).toHaveStyleRule('z-index', 'auto');
+    });
+
+    test('z_0', () => {
+      const component = renderer.create(<Container z_0 />).toJSON();
+      expect(component).toHaveStyleRule('z-index', '0');
+    });
+
+    test('z_10', () => {
+      const component = renderer.create(<Container z_10 />).toJSON();
+      expect(component).toHaveStyleRule('z-index', '10');
+    });
+
+    test('z_20', () => {
+      const component = renderer.create(<Container z_20 />).toJSON();
+      expect(component).toHaveStyleRule('z-index', '20');
+    });
+
+    test('z_30', () => {
+      const component = renderer.create(<Container z_30 />).toJSON();
+      expect(component).toHaveStyleRule('z-index', '30');
+    });
+
+    test('z_40', () => {
+      const component = renderer.create(<Container z_40 />).toJSON();
+      expect(component).toHaveStyleRule('z-index', '40');
+    });
+
+    test('z_50', () => {
+      const component = renderer.create(<Container z_50 />).toJSON();
+      expect(component).toHaveStyleRule('z-index', '50');
+    });
+  });
+
+  describe('Overflow', () => {
+    describe('Auto', () => {
+      test('s_auto', () => {
+        const component = renderer.create(<Container s_auto />).toJSON();
+        expect(component).toHaveStyleRule('overflow', 'auto');
+      });
+
+      test('sy_auto', () => {
+        const component = renderer.create(<Container sy_auto />).toJSON();
+        expect(component).toHaveStyleRule('overflow-y', 'auto');
+      });
+
+      test('sx_auto', () => {
+        const component = renderer.create(<Container sx_auto />).toJSON();
+        expect(component).toHaveStyleRule('overflow-x', 'auto');
+      });
+    });
+
+    describe('Visible', () => {
+      test('s_visible', () => {
+        const component = renderer.create(<Container s_visible />).toJSON();
+        expect(component).toHaveStyleRule('overflow', 'visible');
+      });
+
+      test('sy_visible', () => {
+        const component = renderer.create(<Container sy_visible />).toJSON();
+        expect(component).toHaveStyleRule('overflow-y', 'visible');
+      });
+
+      test('sx_visible', () => {
+        const component = renderer.create(<Container sx_visible />).toJSON();
+        expect(component).toHaveStyleRule('overflow-x', 'visible');
+      });
+    });
+
+    describe('Hidden', () => {
+      test('s_hidden', () => {
+        const component = renderer.create(<Container s_hidden />).toJSON();
+        expect(component).toHaveStyleRule('overflow', 'hidden');
+      });
+
+      test('sy_hidden', () => {
+        const component = renderer.create(<Container sy_hidden />).toJSON();
+        expect(component).toHaveStyleRule('overflow-y', 'hidden');
+      });
+
+      test('sx_hidden', () => {
+        const component = renderer.create(<Container sx_hidden />).toJSON();
+        expect(component).toHaveStyleRule('overflow-x', 'hidden');
+      });
+    });
+  });
+
+  describe('Position', () => {
+    test('fixed', () => {
+      const component = renderer.create(<Container fixed />).toJSON();
+      expect(component).toHaveStyleRule('position', 'fixed');
+    });
+
+    test('absolute', () => {
+      const component = renderer.create(<Container absolute />).toJSON();
+      expect(component).toHaveStyleRule('position', 'absolute');
+    });
+
+    test('relative', () => {
+      const component = renderer.create(<Container relative />).toJSON();
+      expect(component).toHaveStyleRule('position', 'relative');
+    });
+
+    test('sticky', () => {
+      const component = renderer.create(<Container sticky />).toJSON();
+      expect(component).toHaveStyleRule('position', 'sticky');
+    });
+  });
+
+  describe('Placement', () => {
+    test('top', () => {
+      const component = renderer.create(<Container top />).toJSON();
+      expect(component).toHaveStyleRule('top', '0');
+    });
+
+    test('right', () => {
+      const component = renderer.create(<Container right />).toJSON();
+      expect(component).toHaveStyleRule('right', '0');
+    });
+
+    test('bottom', () => {
+      const component = renderer.create(<Container bottom />).toJSON();
+      expect(component).toHaveStyleRule('bottom', '0');
+    });
+
+    test('left', () => {
+      const component = renderer.create(<Container left />).toJSON();
+      expect(component).toHaveStyleRule('left', '0');
+    });
+
+    test('cover', () => {
+      const component = renderer.create(<Container cover />).toJSON();
+      expect(component).toHaveStyleRule('top', '0');
+      expect(component).toHaveStyleRule('right', '0');
+      expect(component).toHaveStyleRule('bottom', '0');
+      expect(component).toHaveStyleRule('left', '0');
+    });
   });
 });
