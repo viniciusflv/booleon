e8d731b03090919dd43ca6a935a647778e68cd1d
Vinícius Victorino <sinqia.vinicius@agibank.com.br>
2021-01-18 10:41:36 -0300

    test(react): ssr

--- tests/react/SSR.test.tsx
+++ tests/react/SSR.test.tsx
@@ -11,11 +11,59 @@ describe('SSR', () => {
     const MyComponent = booleon.section([
       ['flex', () => 'display:flex;'],
     ] as const);
-    const string = ReactDOM.renderToString(
-      <MyComponent data-testid="renderToString" flex />,
-    );
+    const string = ReactDOM.renderToString(<MyComponent flex />);
     expect(string).toBe(
-      '<style data-booleon="bl--1587999785">.bl--1587999785{display:flex;}</style><section class="bl--1587999785" data-testid="renderToString"></section>',
+      '<style data-booleon="bl--1587999785">.bl--1587999785{display:flex;}</style><section class="bl--1587999785"></section>',
     );
+  });
+
+  test('renderToStaticMarkup', () => {
+    const MyComponent = booleon.section([
+      ['flex', () => 'display:flex;'],
+    ] as const);
+    const string = ReactDOM.renderToStaticMarkup(<MyComponent flex />);
+    expect(string).toBe(
+      '<style data-booleon="bl--1587999785">.bl--1587999785{display:flex;}</style><section class="bl--1587999785"></section>',
+    );
+  });
+
+  test('renderToNodeStream', async (done) => {
+    const MyComponent = booleon.section([
+      ['flex', () => 'display:flex;'],
+    ] as const);
+    const stream = ReactDOM.renderToNodeStream(<MyComponent flex />);
+    const string = await new Promise((resolve, reject) => {
+      let received = '';
+      stream.on('data', (chunk) => (received += chunk));
+      stream.on('end', () => resolve(received));
+      stream.on('error', reject);
+    });
+
+    expect(string).toBe(
+      '<style data-booleon="bl--1587999785">.bl--1587999785{display:flex;}</style><section class="bl--1587999785"></section>',
+    );
+
+    done();
+  });
+
+  test('renderToStaticNodeStream', async (done) => {
+    const MyComponent = booleon.section([
+      ['flex', () => 'display:flex;'],
+    ] as const);
+    const staticStream = ReactDOM.renderToStaticNodeStream(
+      <MyComponent flex />,
+    );
+    const string = await new Promise((resolve, reject) => {
+      let received = '';
+      staticStream.on('data', (chunk) => (received += chunk));
+      staticStream.on('end', () => resolve(received));
+      staticStream.on('error', reject);
+    });
+
+    expect(string).toBe(
+      '<style data-booleon="bl--1587999785">.bl--1587999785{display:flex;}</style><section class="bl--1587999785"></section>',
+    );
+
+    done();
   });
 });
