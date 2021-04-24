3c933249c0202d5be7950879efb63f0efdafae13
Vinícius Victorino <sinqia.vinicius@agibank.com.br>
2021-04-24 10:18:04 -0300

    feat(ui): link

---
+++ packages/ui/src/components/Link/Link.tsx
@@ -0,0 +1,20 @@
+import modules from '@booleon/modules';
+import { booleon } from '@booleon/react';
+
+const _Link = booleon.a(modules);
+function Link({ as = 'a', children, ...props }: any) {
+  console.log(as);
+  return (
+    <_Link
+      {...props}
+      as={as}
+      ft_color_inherit
+      ft_no_underline
+      focus__ft_color_d55901
+      hover__ft_color_d55901>
+      {children}
+    </_Link>
+  );
+}
+
+export default Link;
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './Button';
 export * from './Svg';
+export * from './Link';
