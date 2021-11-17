67a8ad3a997953414d1786cb4b0a6f4b4ea158d9
Vinícius Victorino <sinqia.vinicius@agibank.com.br>
2021-11-17 15:18:27 -0300

    refactor(docs): rxm

--- packages/docs/src/containers/Layout/Header/Header.tsx
+++ packages/docs/src/containers/Layout/Header/Header.tsx
@@ -29,25 +29,25 @@ export function Header({ isDocs = false }) {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <_Header h_8vh min_h_60pxrem max_h_80pxrem w_100$>
+    <_Header h_8vh min_h_60rxm max_h_80rxm w_100$>
       <_Nav
         fixed
         z_max
         flex
         h_8vh
-        min_h_60pxrem
-        max_h_80pxrem
+        min_h_60rxm
+        max_h_80rxm
         w_100$
         dark__bdb_1_solid_000
         bdb_1_solid_d5d5d5
         bg_color_fff
         dark__bg_color_22202c
       >
-        <_Div flex grow cross_center max_w_1440px m_auto px_20px>
-          <Link to="/" as={GatsbyLink} first={logo} ft_size_25px>
+        <_Div flex grow cross_center max_w_1440px m_auto px_20rxm>
+          <Link to="/" as={GatsbyLink} first={logo} ft_size_25rxm>
             Booleon
           </Link>
-          <_Div flex grow main_end cross_center gap_20px>
+          <_Div flex grow main_end cross_center gap_20rxm>
             {isDocs ? (
               <Search />
             ) : (
@@ -56,7 +56,7 @@ export function Header({ isDocs = false }) {
               </Link>
             )}
             <Link
-              ft_size_20px
+              ft_size_20rxm
               first={github}
               title="Github link"
               target="__blank"
@@ -70,7 +70,7 @@ export function Header({ isDocs = false }) {
               p_0
               bg_transparent
               ft_color_inherit
-              important__ft_size_30px
+              important__ft_size_30rxm
               hover__cr_pointer
               hover__ft_color_d55901
               focus__ft_color_d55901
