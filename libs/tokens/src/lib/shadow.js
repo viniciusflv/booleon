2ec3abee753283f6c11f7d708dd5a1d58cdb3c16
Vinícius Victorino <sinqia.vinicius@agibank.com.br>
2022-03-11 16:43:36 -0300

    fix(token): elevation 3 shadow

--- libs/tokens/src/lib/shadow.js
+++ libs/tokens/src/lib/shadow.js
@@ -89,7 +89,7 @@ module.exports = {
       3: '0px 3px 1px rgba(0, 0, 0, 0.12)',
     },
     3: {
-      1: '1px 8px 0px rgba(0, 0, 0, 0.2)',
+      1: '0px 1px 8px rgba(0, 0, 0, 0.2)',
       2: '0px 3px 4px rgba(0, 0, 0, 0.14)',
       3: '0px 3px 3px rgba(0, 0, 0, 0.12)',
     },
